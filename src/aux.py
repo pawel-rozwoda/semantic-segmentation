d972a268c046e88f2e1c6164bf8d7572aa0d4b79
pawel-rozwoda <pawel202@protonmail>
2020-01-24 11:56:48 +0100

    move dice loss function to aux.py

--- src/aux.py
+++ src/aux.py
@@ -1,6 +1,7 @@
 import math
 import numpy as np
 from numba import njit
+import torch
 
 def get_x(value, height ):
     return int(value)//height
@@ -57,5 +58,34 @@ def get_indexes(*, arr, height, width): #* stands for requiring keyword args
                 indexes.append(((i*width) + j - counter + 1 , counter))
                 counter = 0
 
-    # print(indexes)
     return indexes 
+
+
+
+
+def soft_dice_loss(y_true, y_pred, epsilon=1e-6):
+    '''
+    Soft dice loss calculation for arbitrary batch size, number of classes, and number of spatial dimensions.
+    Assumes the `channels_last` format.
+
+    # Arguments
+        y_true: b x X x Y( x Z...) x c One hot encoding of ground truth
+        y_pred: b x X x Y( x Z...) x c Network output, must sum to 1 over c channel (such as after softmax)
+        epsilon: Used for numerical stability to avoid divide by zero errors
+
+    # References
+        V-Net: Fully Convolutional Neural Networks for Volumetric Medical Image Segmentation
+        https://arxiv.org/abs/1606.04797
+        More details on Dice loss formulation
+        https://mediatum.ub.tum.de/doc/1395260/1395260.pdf (page 72)
+
+        Adapted from https://github.com/Lasagne/Recipes/issues/99#issuecomment-347775022
+    '''
+
+    # skip the batch and class axis for calculating Dice score
+    axes = tuple(range(1, len(y_pred.shape)-1))
+    numerator = 2. * torch.sum(y_pred * y_true, axes)
+    # denominator = torch.sum(torch.square(y_pred) + torch.square(y_true), axes)
+    denominator = torch.sum(y_pred**2 + y_true**2, axes)
+
+    return 1. - torch.mean(numerator / (denominator + epsilon)) # average over classes and batch 
