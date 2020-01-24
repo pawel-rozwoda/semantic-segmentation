834d08470686bb92179311ca75f4c2915c636cbc
pawel-rozwoda <pawel202@protonmail>
2020-01-24 12:01:26 +0100

    slightly change get_item shape

--- src/load.py
+++ src/load.py
@@ -10,7 +10,7 @@ import matplotlib
 from aux import str_to_mask 
 
 class MyDataset(Dataset):
-    def __init__(self, *, train_dir, labels , transform=None, train, train_partition):
+    def __init__(self, *, train_dir, labels , transform=None, train, train_partition, do_not_scale=False):
         self.train_dir = train_dir
         self.transform = transform 
         self.labels = pd.read_csv(labels)
@@ -29,6 +29,7 @@ class MyDataset(Dataset):
         self.image_width = first_img.shape[1]
         
         self.labels = self.labels.sort_values(by='Image_Label')
+        self.do_not_scale = do_not_scale
 
 
     def __len__(self):
@@ -39,6 +40,17 @@ class MyDataset(Dataset):
 
         img_path = os.path.join(self.train_dir, self.images[idx])
         image = io.imread(img_path).astype('float32')/255.
+        image = torch.Tensor(image)
+
+        if not self.do_not_scale:
+            image = image.unsqueeze(0) 
+            image = image.permute(0,3,1,2)
+            image = torch.nn.functional.interpolate(image,size=(128,128), mode='nearest')
+            image = image.squeeze(0)
+
+        else:
+            image = image.permute(2,0,1)
+        # print('image', image.shape)
 
         df = self.labels
         search_key = os.path.splitext(self.images[idx])[0] + '.jpg'
@@ -54,5 +66,19 @@ class MyDataset(Dataset):
                         df[row_sugar:row_sugar+1]])
  
         label=str_to_mask(df_rows=rows, height =  self.image_height, width = self.image_width) 
+        label=torch.Tensor(label)
+    
+        if not self.do_not_scale:
+            label = label.unsqueeze(0)
+            label = torch.nn.functional.interpolate(label,size=(128,128), mode='nearest')
+            label = label.squeeze(0)
+
+        # print('label', label.shape)
+        # print('-----')
+        # print(torch.sum(label==0.) + torch.sum(label==1))
+        # print(np.prod(label.shape))
+        # print('-----')
+        # print()
+
         
         return image, label
